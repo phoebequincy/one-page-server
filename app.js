f56d4080b1eb4f18f04f74bd1bbfcd8a8ea9e427
Phoebe Quincy <phoebequincy@gmail.com>
2018-11-20 15:35:24 -0700

    changed messages in app.js and got server started

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+// const listener = () => console.log(`Application listening on port ${port}!`)
+// app.listen(port, listener)
+const listener = () => console.log(`Woot! The party's here! ${port}!`)
 app.listen(port, listener)
+app.get('/ping', (req, res) => {
+  const message = 'pong!'
+  console.log(`ON THE SERVER: ${message}`)
+  res.send(`RESPONDING TO THE CLIENT: ${message}`)
+})
