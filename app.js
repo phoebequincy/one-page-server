a7f1f73243166da156e7a70730ef129db21f738b
Phoebe Quincy <phoebequincy@gmail.com>
2018-11-25 16:46:25 -0700

    Used REST commands to update server

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const port = 3000
 // app.listen(port, listener)
 const listener = () => console.log(`Woot! The party's here! ${port}!`)
 app.listen(port, listener)
+// app.get('/ping', (req, res) => {
+//   const message = 'pong!'
+//   console.log(`ON THE SERVER: ${message}`)
+//   res.send(`RESPONDING TO THE CLIENT: ${message}`)
 app.get('/ping', (req, res) => {
   const message = 'pong!'
   console.log(`ON THE SERVER: ${message}`)
-  res.send(`RESPONDING TO THE CLIENT: ${message}`)
+  res.setHeader('content-type', 'text/html');
+  res.send(`<h1>Now you're cooking with oil!</h1>`)
 })
